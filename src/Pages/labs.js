94d57750a3455f775dcbd8587b0d97fd320cf47d
Jayesh Mishra <jm6125523@gmail.com>
2023-01-06 14:33:12 +0530

    Minor Changes to Folder Structure

--- src/Pages/labs.js
+++ src/Pages/labs.js
@@ -13,37 +13,37 @@ const Labs = () => {
     swipeToSlide: true,
   };
   return (
-      <div className="service-container">
-        <h1>Labs</h1>
-        <Slider {...settings}>
-          <div className="service-card">
-            <img
-              className="service-carousel-img"
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmKs2Pvi5ceXcKXp_4-f9rbgY-a9Rco6JYNQ&usqp=CAU"
-              alt="im"
-            />
-            <p>Get_Cure_Labs</p>
-          </div>
+    <div className="service-container">
+      <h1>Labs</h1>
+      <Slider {...settings}>
+        <div className="service-card">
+          <img
+            className="service-carousel-img"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmKs2Pvi5ceXcKXp_4-f9rbgY-a9Rco6JYNQ&usqp=CAU"
+            alt="im"
+          />
+          <p>Get_Cure_Labs</p>
+        </div>
 
-          <div className="service-card">
-            <img
-              className="service-carousel-img"
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlnjntkyg8occonN07D7hZ-ITIzOEfUb2O_w&usqp=CAU"
-              alt="im"
-            />
+        <div className="service-card">
+          <img
+            className="service-carousel-img"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlnjntkyg8occonN07D7hZ-ITIzOEfUb2O_w&usqp=CAU"
+            alt="im"
+          />
 
-            <p>Medi_Cure_Labs</p>
-          </div>
-          <div className="service-card">
-            <img
-              className="service-carousel-img"
-              src="https://img.freepik.com/free-photo/hand-with-protective-gloves-holding-blood-samples-covid-test_23-2148958363.jpg?w=2000"
-              alt="im"
-            />
-            <p>Your_Doc_Labs</p>
-          </div>
-        </Slider>
-      </div>
+          <p>Medi_Cure_Labs</p>
+        </div>
+        <div className="service-card">
+          <img
+            className="service-carousel-img"
+            src="https://img.freepik.com/free-photo/hand-with-protective-gloves-holding-blood-samples-covid-test_23-2148958363.jpg?w=2000"
+            alt="im"
+          />
+          <p>Your_Doc_Labs</p>
+        </div>
+      </Slider>
+    </div>
   );
 };
 
